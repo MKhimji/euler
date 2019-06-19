939609496cea28f69aa92af6eca3cbf88a38b2bd
MKhimji <muhammadkhimji@live.co.uk>
2019-06-19 05:05:51 +0000

    prob7

---
+++ prob7.py
@@ -0,0 +1,18 @@
+"""By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number"""
+
+def prob7():
+    n=2
+    i=2
+    while n<13:
+        if n % i == 0:
+            n+=1
+        if n % i != 0:
+            i+=
+
+
+
+
+
+
